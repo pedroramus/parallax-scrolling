932add8ef82d6fb64419612e071d2daa96232c47
pedroramus <p.i.c.m.ramos@gmail.com>
2016-08-17 15:02:37 -0300

    effect link clicked

--- project-2/js/parallax.js
+++ project-2/js/parallax.js
@@ -2,6 +2,42 @@ $(document).ready(function(){
     $(window).bind('scroll', function(e){
         parallax();
     });
+    
+    $('a.earth').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000, function(){
+            parallax();
+        });
+        return false;
+    });
+    
+    $('a.moon').click(function() {
+        $('html, body').animate({
+            scrollTop: $('#moon').offset().top
+        }, 1000, function(){
+            parallax();
+        });
+        return false;
+    });
+    
+    $('a.rocket').click(function() {
+        $('html, body').animate({
+            scrollTop: $('#rocket').offset().top
+        }, 1000, function(){
+            parallax();
+        });
+        return false;
+    });
+    
+    $('a.saturn').click(function() {
+        $('html, body').animate({
+            scrollTop: $('#saturn').offset().top
+        }, 1000, function(){
+            parallax();
+        });
+        return false;
+    });
 });
 function parallax() {
     var scrollPosition = $(window).scrollTop();
